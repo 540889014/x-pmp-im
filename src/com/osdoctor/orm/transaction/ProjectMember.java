567015f13a0f0fccec0f57ab4f30dd306c4032f7
小小 <x-man@xiaoxiaodeAir.lan>
2017-04-12 20:39:45 +0800

    any

---
+++ src/com/osdoctor/orm/transaction/ProjectMember.java
@@ -0,0 +1,12 @@
+package com.osdoctor.orm.transaction;
+
+import com.osdoctor.orm.common.UserInfo;
+
+public class ProjectMember {
+
+	private UserInfo userInfo;
+	private String dutyName;//职责
+	private ProjectInfo projectInfo;
+	private String dutyDetail;
+	
+}
