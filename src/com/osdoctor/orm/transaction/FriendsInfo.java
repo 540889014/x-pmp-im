da7c96002f691fa0edd8120f05a08915f6386612
540889014 <540889014@qq.com>
2017-04-12 20:42:48 +0800

    a

--- src/com/osdoctor/orm/transaction/FriendsInfo.java
+++ src/com/osdoctor/orm/transaction/FriendsInfo.java
@@ -1,5 +1,24 @@
 package com.osdoctor.orm.transaction;
 
+import java.util.List;
+
+import com.osdoctor.orm.common.UserInfo;
+
 public class FriendsInfo {
 
+	private UserInfo userInfo;
+	private List<UserInfo> myFriends;
+	public UserInfo getUserInfo() {
+		return userInfo;
+	}
+	public void setUserInfo(UserInfo userInfo) {
+		this.userInfo = userInfo;
+	}
+	public List<UserInfo> getMyFriends() {
+		return myFriends;
+	}
+	public void setMyFriends(List<UserInfo> myFriends) {
+		this.myFriends = myFriends;
+	}
+	
 }
